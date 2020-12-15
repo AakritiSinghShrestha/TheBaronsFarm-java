1d3755d33a325cdb261add8edea003ccc8ddfb97
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-12-15 15:47:28 -0500

    created a person class. worked on a variable,constructor and getter and setter method

--- src/main/java/hbcu/stay/ready/baronsfarm/concrete/Person.java
+++ src/main/java/hbcu/stay/ready/baronsfarm/concrete/Person.java
@@ -1,4 +1,17 @@
 package hbcu.stay.ready.baronsfarm.concrete;
 
 public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

fd95d42e78db27950786e3cb7946ef65f2fd78bf
AakritiShresthaShrestha <Aakritibikash.stha88@gmail.com>
2020-12-15 15:43:58 -0500

    Added interface class

---
+++ src/main/java/hbcu/stay/ready/baronsfarm/Interface/Botanist.java
@@ -0,0 +1,5 @@
+package hbcu.stay.ready.baronsfarm.Interface;
+
+public interface Botanist {
+    public void plant(Crop cropln);
+}
---
+++ src/main/java/hbcu/stay/ready/baronsfarm/Interface/Rideable.java
@@ -0,0 +1,6 @@
+package hbcu.stay.ready.baronsfarm.Interface;
+
+public interface Rideable {
+    public void mount(Rider);
+    String dismount();
+}
